8bac39a92cf479613410804e7c8ae27cca91f69e
Ahmet Gürdal <ahmetgurdal32@gmail.com>
2015-04-13 03:40:10 +0300

    Update rand.py

--- rand.py
+++ rand.py
@@ -1,12 +1,19 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# -*- coding: cp1254 -*-
 import random
-print "-This is a program to chose players randomly in a tournament."
+print "INSTRUCTIONS"
+print "-------------------------------------------------------------"
+print "-This is a program for chose players randomly in a tournament."
 print "-First you have to insert all players name."
 print "-Then you have to insert players who win their game."
-print "-If first player win the game press a or B"
-print "-If other one win the game press b or B"
+print "-When all player's names added ,type 'end' to finish adding."
+print "-If first player win the game press 1"
+print "-If other one win the game press 2"
 print "-For Example 'PlayerA -- PlayerB' is one of chosen players."
-print "-If PlayerA won the game type a or A"
-print "-Else type b or B"
+print "-If PlayerA won the game type 1"
+print "-Else type 2"
+print "--------------------------------------------------------------"
 liste = []
 while True:
 	player = raw_input("Insert Player Name:")
@@ -15,9 +22,11 @@ while True:
 	else:
 		liste.append(player)
 
-yeni = liste
+retry = []
+for i in liste:
+	retry.append(i)
+next = []
 while True:
-	liste = yeni
 	liste2 = []
 	while True:
 		winlist = []
@@ -28,32 +37,39 @@ while True:
 				continue
 			playera = liste[a]
 			playerb = liste[b]
+			if len(next) != 0:
+				for i in next:
+					if i in liste:
+						playera = i
+			if playera == playerb:
+				while playera == playerb:
+					playerb = liste[random.randint(0,len(liste) - 1)]
 			print playera + "--" + playerb
 			liste.remove(playera)
 			liste.remove(playerb)
 			liste2.append(playera)
 			liste2.append(playerb)
 		if len(liste) == 1:
+			next.append(liste[0])
 			winlist.append(liste[0])
 			print "Next tour : " + liste[0]
 		liste3 = liste2	
 		print "-------------"
 		while len(liste2) >= 2:
 			who = raw_input(liste2[0] + "--" + liste2[1] + ":")
-			if who == "a" or who == "A":
+			if who == "1":
 				winlist.append(liste2[0])
 				liste2.remove(liste2[0])
 				liste2.remove(liste2[0])
 				
 			
-			elif who == "b" or who == "B":
+			elif who == "2":
 				winlist.append(liste2[1])
 				liste2.remove(liste2[0])
 				liste2.remove(liste2[0])
 				
 			else:
 				continue
-
 		if len(winlist) == 1:
 			print "The winner is " + winlist[0]
 			break
@@ -65,9 +81,11 @@ while True:
 			winlist.append(liste2[0])
 			liste = winlist
 	print "GAME OVER"
-	ret = raw_input("Retry? (r/R):")
-	if ret == "r" or ret == "R":
-		print "Same players"
+	ret = raw_input("Do you wanna restart with Same players? (y/n):")
+	if ret == "y" or ret == "Y":
+		for i in retry:
+			liste.append(i)
 		print "--------------"
 	else:
+		print "I hope u enjoyed"
 		break
